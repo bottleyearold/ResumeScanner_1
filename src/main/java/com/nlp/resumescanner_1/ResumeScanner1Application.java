5b8b906dac3db34e76ee5665ea6a0a8c8217698b
bottleyearold <fionamageeva@yahoo.com>
2024-10-03 11:02:26 -0400

    test

---
+++ src/main/java/com/nlp/resumescanner_1/ResumeScanner1Application.java
@@ -0,0 +1,13 @@
+package com.nlp.resumescanner_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeScanner1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResumeScanner1Application.class, args);
+    }
+
+}
